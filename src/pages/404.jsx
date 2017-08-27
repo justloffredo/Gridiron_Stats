9078e33bd678b9109851247ed5e2a0e7b61dd47f
Justin Loffredo <justloffredo@gmail.com>
2017-08-27 12:40:26 -0400

    react-redux-scaffolding

---
+++ src/pages/404.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { connect } from "redux-thunk";
+import "./404.scss";
+
+class FourOhFour extends Component {
+	render() {
+		return (
+			<div className = "404">
+				<h1>This is the 404 page</h1>
+			</div>
+		);
+	}
+}
+
+export default FourOhFour;
