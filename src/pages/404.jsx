481548b6067207eb0f2c8a647d4f5dcaf10167ca
Justin Loffredo <justloffredo@gmail.com>
2017-08-27 20:06:47 -0400

    Primitive navbar is up

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { connect } from "redux-thunk";
+// import { connect } from "redux-thunk";
 import "./404.scss";
 
 class FourOhFour extends Component {
