44602ba72f5bf473b81423db622153b8879310ae
Samir Dhebar <samirdhebar@gmail.com>
2017-08-28 21:19:28 -0400

    adding home page plus minor styling without parallax implementation...yet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,32 @@ import "./Home.scss";
 class Home extends Component {
 	render() {
 		return (
-			<div className = "Home">
-				<h1>This is the Home page</h1>
+
+			<div className="container">
+				<section className="background">
+					<div className="content-wrapper">
+						<p className="content-title">gridiron stats</p>
+						<p className="content-subtitle">See where your team ranks!</p>
+					</div>
+				</section>
+				<section className="background">
+					<div className="content-wrapper">
+						<p className="content-title">do a comparison</p>
+						<p className="content-subtitle">Check where your team ranks against the competition</p>
+					</div>
+				</section>
+				<section className="background">
+					<div className="content-wrapper">
+						<p className="content-title">donate</p>
+						<p className="content-subtitle">support charities affected by injustice worldwide</p>
+					</div>
+				</section>
 			</div>
+
+
 		);
 	}
 }
+
 
 export default Home;
