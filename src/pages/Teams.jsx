5498a33d223791a13c481366aa515c1abd73891f
Justin Loffredo <justloffredo@gmail.com>
2017-08-28 20:12:52 -0400

    created action file for teams and updated team.jsx to render a dropdown

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,15 +1,48 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import nflTeamsList from "json/dropdown.json";
 import "./Teams.scss";
 
 class Teams extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			team1: "",
+			team2: "",
+		};
+	}
+
+	_handleChange = (event) => {
+		this.setState({
+			[event.target.name]: [event.target.value],
+		});
+	}
+
+	_handleSubmit = (event) => {
+		event.preventDefault();
+		this.props.submit(this.state);
+	}
+
 	render() {
 		return (
-			<div className = "Teams">
-				<h1>This is the Teams Page</h1>
+
+
+			<div className= "team-one-submit">
+				<label name="team1"> Team 1 </label>
+				<select onChange= {this.handleChange} name = "team1" >
+					{nflTeamsList.map((teams) => {
+						return (
+							<option value= { teams.abr }>{ teams.name }</option>
+						);
+					})}
+				</select>
+				<div className="button-field-submit hidden">
+					<button type="submit" onSubmit={this._handleSubmit}>Submit</button>
+				</div>
 			</div>
 		);
 	}
 }
+
 
 export default Teams;
